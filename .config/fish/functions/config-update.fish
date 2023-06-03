37918d7e7786da4d2aed5d651a519a786bab0f3c
Manuel Fuchs <manueltfuchs@gmail.com>
2023-06-03 11:45:17 +0200

    Update /Users/manuelfuchs/.config/fish/config.fish /Users/manuelfuchs/.config/fish/functions/config-update.fish

---
+++ .config/fish/functions/config-update.fish
@@ -0,0 +1,16 @@
+function config-update
+  if [ $argv = "fish" ]
+    set --function config_files ~/.config/fish/config.fish ~/.config/fish/functions/*
+  else if [ $argv = "brew" ]
+    brew bundle dump --file=$HOME/Brewfile --force
+    set --function config_files ~/Brewfile
+  else if [ $argv = "asdf" ]
+    set --function config_files $HOME/.asdfrc
+  else
+    set --function config_files $argv
+  end
+
+  config add $config_files
+  config commit -m "Update $config_files"
+  config push &> /dev/null
+end
